17d55ff06217db27861a9e4fc7711e3435a9814c
FirstSlip <86049908+FirstSlip@users.noreply.github.com>
2021-12-27 18:51:20 +0500

    Initial commit

---
+++ dz1.py
@@ -0,0 +1,81 @@
+from flask import Flask
+from flask import render_template
+from flask import Response
+import sqlite3
+import random
+import io
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def cv_index():
+    cvs = get_cv()
+    res = ""
+    for i, cv in enumerate(cvs):
+        res += f"<h1>{i + 1})</h1>"
+        res += f"<p>Желаемая зарплата: {cv['salary']}.</p>"
+        res += f"<p>Образование: {cv['educationType']}.</p>"
+
+    return res
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('d3-dz.html',
+                           cvs=get_cv(), work=get_work(), salary=get_salary(), length = len(get_cv()),
+                           )
+
+
+def dict_factory(cursor, row):
+    # обертка для преобразования
+    # полученной строки. (взята из документации)
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+def get_cv():
+    con = sqlite3.connect('works.sqlite')
+    con.row_factory = dict_factory
+    res = list(con.execute('select * from works limit 20'))
+    con.close()
+    return res
+
+def get_work():
+    work = ''
+    cvs = get_cv()
+    for cv in cvs:
+        work += cv['work'] + " "
+    return work
+
+def get_salary():
+    salary = []
+    cvs = get_cv()
+    for cv in cvs:
+        salary.append(cv['salary'])
+    return salary
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+
+print(get_work())
+print(get_salary())
+app.run()
